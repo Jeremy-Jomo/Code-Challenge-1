dede4584faee144e3c3d54d4d54af79ed15ff118
Jeremy <markjeremyj@gmail.com>
2025-06-07 20:01:16 +0300

    Adds boda far calculating function

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,1 +1,10 @@
+const distance = prompt('Unafika wapi Mkubwa?\nKilometer ngapi?:');
+function calculateBodaFare(distance){
+    console.log(`Uko kwote? Io ni ${distance} km:
+Ukikalia Pikipiki: KES 50
+Mpaka Uko: KES ${distance * 15}
+Total: KES ${(distance*15)+50}
 
+Panda Pikipiki!`)
+}
+calculateBodaFare(distance)
