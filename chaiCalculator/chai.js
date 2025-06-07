c3804e27afe1ba9dad8e4c57c0cce8e6a1ca5790
Jeremy <markjeremyj@gmail.com>
2025-06-07 19:39:37 +0300

    Adds chai ingedient calculator

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,1 +1,12 @@
+const cups = prompt('Karibu! How many cups of Chai Bora would you like to make?')
 
+function calculateChaiIngredients(cups){
+    console.log(`To make ${cups} cups of Kenyan Chai, you will need:
+Water: ${cups * 200} ml
+Milk: ${cups * 50} ml
+Tea Leaves (Majani): ${cups} tablespoons
+Sugar (Sukari): ${cups *2} teaspoons
+
+Enjoy your Chai Bora!`)
+}
+calculateChaiIngredients(cups)
